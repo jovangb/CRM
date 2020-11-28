37376a4234f28519abe3530650957e1844530cd5
Inconfessus <50893976+Inconfessus@users.noreply.github.com>
2020-11-28 16:25:58 -0600

    Ingresar Citas

---
+++ CRM/admin/site/elements/formulario_citas.php
@@ -0,0 +1,107 @@
+<div class="card card-primary">
+  <div class="card-header">
+    <h3 class="card-title">Citas</h3>
+  </div>
+  <!-- /.card-header -->
+  <!-- form start -->
+  <form>
+    <div class="card-body">
+
+      <!-- 
+  tipo
+  nombre
+  calle_uno
+  calle_dos
+  ciudad
+  estado
+  CP
+  Teléfono
+  Celular
+  correo
+ -->
+
+      <div class="row">
+        <div class="col-sm">
+
+            <div class="form-group">
+                <label for="exampleInputPassword1">Contacto</label>
+                    <select class="form-control">
+                        <option selected disabled hidden>Contacto</option>
+                        <option value="Contacto">Jorge Baidon</option>
+                        <option value="Contacto">Eduardo Cortés</option>
+                    </select>
+            </div>
+
+          <div class="form-group">
+            <label for="exampleInputEmail1">Descripcion</label>
+                <div class="form-group">
+                    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Descripcion">
+                </div>
+
+          </div>
+
+          
+
+          <!-- end of column -->
+        </div>
+
+        <div class="col-sm">
+
+        <div class="row">
+            <div class="col-sm">
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Tipo</label>
+                        <select class="form-control">
+                            <option selected disabled hidden>Tipo</option>
+                            <option value="Venta">Venta</option>
+                            <option value="Proveedor">Proveedor</option>
+                        </select>
+                </div>
+
+                <div class="form-group">
+                  <label>Fecha</label>
+                    <div class="input-group date" id="reservationdate" data-target-input="nearest">
+                        <input type="text" class="form-control datetimepicker-input" data-target="#reservationdate"/>
+                        <div class="input-group-append" data-target="#reservationdate" data-toggle="datetimepicker">
+                            <div class="input-group-text"><i class="fa fa-calendar"></i></div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="col-sm">
+
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Cantidad</label>
+                    <input type="text" class="form-control" id="cantidad" placeholder="Cantidad">
+                </div>
+
+            </div>
+
+          </div>
+
+          <div class="row">
+            <div class="col-sm">
+           
+              <div class="form-group">
+                
+              </div>
+            </div>
+
+            <div class="col-sm">
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+
+    </div>
+    <!-- /.card-body -->
+
+    <div class="card-footer">
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </div>
+  </form>
+</div>
---
+++ CRM/admin/site/scripts/js-citas.php
@@ -0,0 +1,11 @@
+<!-- InputMask -->
+<script src="../plugins/moment/moment.min.js"></script>
+<script src="../plugins/inputmask/jquery.inputmask.min.js"></script>
+<!-- date-range-picker -->
+<script src="../plugins/daterangepicker/daterangepicker.js"></script>
+<!-- Tempusdominus Bootstrap 4 -->
+<script src="../plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+
+
+
+<script src="../plugins/fullcalendar/fechas.js"></script>
