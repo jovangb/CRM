d0fd2ad85f6a150b2a3bbbcaac68f9d26e8ecfdb
Inconfessus <50893976+Inconfessus@users.noreply.github.com>
2020-11-27 20:03:53 -0600

    Calendario ahora si

--- CRM/CRM/admin/site/scripts/js-calendario.php
+++ CRM/CRM/admin/site/scripts/js-calendario.php
@@ -1,4 +1,4 @@
 <script src="../plugins/moment/moment.min.js"></script>
 <script src="../plugins/fullcalendar/main.js"></script>
-
+<script src="../plugins/jquery-ui/jquery-ui.min.js"></script>
 <script src="../plugins/fullcalendar/calendario.js"></script>
