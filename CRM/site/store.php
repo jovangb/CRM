1e4f6fab63e3d541aad1b53c95dbaefded7253f0
Jovan <gbjovan@gmail.com>
2020-11-30 00:18:57 -0600

    Cambiar  store al maquetado para el vídeo

--- CRM/site/store.php
+++ CRM/site/store.php
@@ -7,22 +7,132 @@
 
     <?php include "elements/header.php" ?>
 
-    <section class="container-fluid min-vh-100">
-        <section class="container h-100" id="main">
-
-            <section class="row">
-                <?php include "elements/searchbar.php" ?>
-            </section>
-            <section id="outer-items">
-             <!-- La parte interior de la iteración de los items debe de estar hecha por etiquetas como las de abajo -->
-                <section id="items" class="row mb-2">
-
-                </section>
-            </section>
-
-        </section>
-
-    </section>
+    <div class="container mb-3" id="main">
+        <div class="row">
+            <div class="col">
+                <form class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="Buscar regalo para enviar a México" aria-label="Recipient's username" aria-describedby="basic-addon2">
+                    <div class="input-group-append">
+                      <button class="btn btn-outline-secondary" type="button">Buscar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div class="row mb-2">
+            <div class="col">
+               <a href="producto-1.php">
+                <div class="card mb-2">
+                    <img src="../images/planta-1.jpg" alt="orquidea" class="img-fluid">
+                    <div class="card-body text-center">
+                        <p>
+                            <small class="text-success">Entrega sin costo, El mísmo día, Todos los días</small><br><br>
+                            Doble Orquídea Morada <br><br>
+                            MX$ 589.00 <br><br>
+                            ★★★★
+                        </p>
+                    </div>
+                </div>
+               </a> 
+            </div>
+            <div class="col">
+                <a href="producto-2.php">
+                    <div class="card">
+                        <img src="../images/planta-2.jpg" alt="orquidea" class="img-fluid">
+                        <div class="card-body text-center">
+                            <p>
+                                <small class="text-success">Entrega sin costo, El mísmo día, Todos los días</small><br><br>
+                                Doble Orquídea Blanca <br><br>
+                                MX$ 599.00 <br><br>
+                                ★★★★★
+                            </p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col">
+                <a href="producto-3.php">
+                    <div class="card">
+                    <img src="../images/planta-3.jpg" alt="orquidea" class="img-fluid">
+                    <div class="card-body text-center">
+                        <p>
+                            <small class="text-success">Entrega sin costo, El mísmo día, Todos los días</small><br><br>
+                            Anturio en Maceta <br><br>
+                            MX$ 499.00 <br><br>
+                            ★★★★
+                        </p>
+                    </div>
+                </div>
+                </a>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <img src="../images/planta-4.jpg" alt="orquidea" class="img-fluid">
+                    <div class="card-body text-center">
+                        <p>
+                            <small class="text-success">Entrega sin costo, El mísmo día, Todos los días</small><br><br>
+                            Palo de Brazil <br><br>
+                            MX$ 269.00 <br><br>
+                            ★★★★★
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row mb-2">
+            <div class="col">
+                <div class="card">
+                    <img src="../images/planta-1.jpg" alt="orquidea" class="img-fluid">
+                    <div class="card-body text-center">
+                        <p>
+                            <small class="text-success">Entrega sin costo, El mísmo día, Todos los días</small><br><br>
+                            Doble Orquídea Morada <br><br>
+                            MX$ 589.00 <br><br>
+                            ★★★★
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <img src="../images/planta-2.jpg" alt="orquidea" class="img-fluid">
+                    <div class="card-body text-center">
+                        <p>
+                            <small class="text-success">Entrega sin costo, El mísmo día, Todos los días</small><br><br>
+                            Doble Orquídea Blanca <br><br>
+                            MX$ 599.00 <br><br>
+                            ★★★★★
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <img src="../images/planta-3.jpg" alt="orquidea" class="img-fluid">
+                    <div class="card-body text-center">
+                        <p>
+                            <small class="text-success">Entrega sin costo, El mísmo día, Todos los días</small><br><br>
+                            Anturio en Maceta <br><br>
+                            MX$ 499.00 <br><br>
+                            ★★★★
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <img src="../images/planta-4.jpg" alt="orquidea" class="img-fluid">
+                    <div class="card-body text-center">
+                        <p>
+                            <small class="text-success">Entrega sin costo, El mísmo día, Todos los días</small><br><br>
+                            Palo de Brazil <br><br>
+                            MX$ 269.00 <br><br>
+                            ★★★★★
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     <!-- hay que reparar el footer que se ve del pito -->
     <?php include "elements/footer.php" ?>
 
